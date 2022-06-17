b72f42357fee65065fbcf028ac68bd6449460cfc
Nikolay Dimitrov <nikolay.joburg@gmail.com>
2022-06-17 18:21:13 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <h2>Portfolio Project</h2>    
+    <><Navbar />
+
+    <section class="hero">
+      <div class="container">
+        <div class="row p-5">
+          <div class="col p-3">
+            <h4>About me</h4>
+              <li>- I am enjoying working with technology</li>
+              <li>- I am enjoying problem-solving</li>
+              <li>- My dream job as a kid was a programmer</li>
+          </div>
+          <div class="col p-3">
+            <h4>Why DevTech?</h4>
+              <li>- To become a Blockchain Developer</li>
+              <li>- To work with the best</li>
+              <li>- To learn from the best</li>
+          </div>
+        </div>
+     </div>
+    </section>
+            
+
+    <Footer /></>    
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+
+    <footer className="p-2 bg-dark text-white text-center mx-auto">
+        <div className="container">
+          <p>&copy;Copyright 2022, Created by Nikolay Dimitrov</p>
+        </div>
+    </footer>
+
+  )
+}
+
+export default Footer
